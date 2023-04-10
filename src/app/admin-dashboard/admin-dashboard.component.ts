81877f084990ae2856853f3565c49df9a5dcff5c
gopisada92 <gopisada92@gmail.com>
2023-04-10 14:58:34 +0530

    LogoutFunctionality

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild ,AfterViewInit} from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import {BreakpointObserver} from '@angular/cdk/layout';
+import { AuthenticationServiceService } from '../authentication-service.service';
+import { Router } from '@angular/router';
+import { AppLoaderService } from '../apploader/apploader.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -8,5 +11,16 @@ import {BreakpointObserver} from '@angular/cdk/layout';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent { 
-
+  constructor(private authservice: AuthenticationServiceService,
+    private loader:AppLoaderService,
+    private router:Router){}
+  logOut(){
+    this.loader.open("Please wait .... ");
+       this.router.navigate(["/"]);
+       setTimeout(() => {      
+          this.loader.close();
+         
+        }, 1000);
+    this.authservice.logout();
+  }
 }
